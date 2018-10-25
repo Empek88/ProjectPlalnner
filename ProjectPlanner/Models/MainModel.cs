ff2caafbc2b65e68e626b2c7bd07ccd6a516d08d
Empek88 <panfilm@wp.pl>
2018-10-25 21:39:01 +0200

    Add MVC folders

---
+++ ProjectPlanner/Models/MainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ProjectPlanner.Models
+{
+    public class MainModel
+    {
+        public int ID { get; set; }
+        public string Descriptiopm { get; set; }
+        public  string Comments { get; set; }
+    }
+}
